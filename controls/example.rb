d564ffc0d955c1b98f4f36474bd700cb14ef2309
c4seyla <c4seyla@gmail.com>
2020-04-06 09:19:53 +0100

    Update example.rb

--- controls/example.rb
+++ controls/example.rb
@@ -1,4 +1,4 @@
-describe aws_s3_bucket(bucket_name: 'inspectest-netpath') do
+describe aws_s3_bucket(bucket_name: 'inspec-demo-fca') do
   it { should exist }
   its('bucket_policy') { should be_empty }
 end
