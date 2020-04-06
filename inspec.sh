6d0b9fa443914d97bf6e95cde852fc66398532ca
c4seyla <c4seyla@gmail.com>
2020-04-06 17:00:24 +0100

    Update inspec.sh

--- inspec.sh
+++ inspec.sh
@@ -1,5 +1,5 @@
 inspec exec -t aws://eu-west-2 ../s3inspec
 if [[ $? -eq 0 ]]
 then
-    aws servicecatalog create-product --cli-input-json file://create-product-input.json
+    aws servicecatalog create-product --cli-input-json file://create-provisioning-artifact-input.json
 fi
