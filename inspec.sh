18dbbf9e47b56b0a95e2c47a8c2c84806a6a7d83
c4seyla <c4seyla@gmail.com>
2020-04-07 08:02:00 +0100

    Update inspec.sh

--- inspec.sh
+++ inspec.sh
@@ -1,5 +1,12 @@
 inspec exec -t aws://eu-west-2 ../s3inspec
 if [[ $? -eq 0 ]]
 then
-    aws servicecatalog create-product --cli-input-json file://create-provisioning-artifact-input.json
+    aws servicecatalog create-product --cli-input-json file://create-provisioning-artifact-input.json > output.json
+    productid = cat output.json grep "ProductId" | awk -F '"' '{print $4}'
+    aws servicecatalog associate-product-with-portfolio
+     --product-id $productid
+     --portfolio-id port-2s6abcdef5wdh4
+    
+else
+     echo "The template failed the Inspec Test"
 fi
