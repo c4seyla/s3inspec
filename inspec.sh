02ec0c2455bbdfc1c2fa4efc792dfa375176c262
c4seyla <c4seyla@gmail.com>
2020-04-07 08:13:28 +0100

    Update inspec.sh

--- inspec.sh
+++ inspec.sh
@@ -2,7 +2,7 @@ inspec exec -t aws://eu-west-2 ../s3inspec
 if [[ $? -eq 0 ]]
 then
     aws servicecatalog create-product --cli-input-json file://create-provisioning-artifact-input.json > output.json
-    productid = cat output.json grep "ProductId" | awk -F '"' '{print $4}'
+    productid=$(cat output.json grep "ProductId" | awk -F '"' '{print $4}')
     echo $productid
     aws servicecatalog associate-product-with-portfolio --product-id $productid --portfolio-id port-2cbakduzuk3vk
     
